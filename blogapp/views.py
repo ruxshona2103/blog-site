80b9c90c0b04fed0af4ac8c9801fd1027b7be0cd
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-01-06 20:21:28 +0500

    Added views.py file

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = "blog/post/list.html"
+
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug, status="published", publihed__year = year, published__month = month, published__day = day)
+    return render(request, 'blog/post/detail.html', {'post':post})
+
