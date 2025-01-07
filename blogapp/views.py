e61574a585fd218da176eb53a6958fd3c0bd5bca
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-01-07 21:09:59 +0500

    Added views.py file

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -11,6 +13,52 @@ class PostListView(ListView):
 
 
 def post_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post, slug=slug, status="published", publihed__year = year, published__month = month, published__day = day)
-    return render(request, 'blog/post/detail.html', {'post':post})
+    post = get_object_or_404(Post, slug=slug,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+    print(comments)
+    new_comment = None
 
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   })
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            title = f"{cd['name']} sizga {post.title}ni o'qishni tafsiya etadi"
+            message = f"{post.title} maqolasini o'qing: {post_url}\n\n" \
+                      f"{cd['name']} ning izohi : {cd['comments']}"
+            send_mail(title, message, 'ruxshonaaglamkhujayeva@gmail.com', [cd['to']], fail_silently=False)
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
