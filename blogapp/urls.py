790723dcbb461bae0cc3ec784b7ba1eeb922f8f3
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-01-07 21:14:19 +0500

    Added templates directory

---
+++ blogapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PostListView, post_detail, post_share
+app_name = 'blog'
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+            post_detail,
+         name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share')
+]
