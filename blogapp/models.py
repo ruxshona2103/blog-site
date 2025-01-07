a46644e7d99560c67653527cba2f579f4c424ef5
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-01-07 21:16:09 +0500

    Updated models.py

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
 
-class PublishedManager(models.Model):
+
+class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status = 'publish')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -22,9 +23,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-
     class Meta:
-        ordering = ("-publish", )
+        ordering = ("-publish",)
 
     def __str__(self):
         return self.title
@@ -33,11 +33,24 @@ class Post(models.Model):
     published = PublishedManager()
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args= [self.publish.unique_for_year,
-                                                        self.publish.month,
-                                                        self.publish.day,
-                                                        self.publish.slug])
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
-posts = Post.objects.filter(status = "published")
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'comment by {self.name} on {self.post}'
